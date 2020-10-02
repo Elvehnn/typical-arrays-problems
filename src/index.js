c8befc877daebeef4bf52a2c267edbd9ce2a0325
Elena Shashina <elvehnn@yandex.ru>
2020-10-02 17:24:30 +0300

    feat: implement func of max and min value of array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array) || !array.length) {
+    alert (array, array.length)
+    return 0;
+   } else {
+      let minElement = array[0];
+      for (let i = 1; i < array.length; i++) {
+        if (array[i] < minElement) {
+          minElement = array[i];
+          }
+      }
+      return minElement;
+  }
 }
 
+
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array) || !array.length) {
+    alert (array, array.length)
+    return 0;
+   } else {
+      let maxElement = array[0];
+      for (let i = 1; i < array.length; i++) {
+        if (array[i] < maxElement) {
+          maxElement = array[i];
+          }
+      }
+      return maxElement;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array) || !array.length) {
+    alert (array, array.length)
+    return 0;
+   } else {
+      let minElement = array[0];
+      for (let i = 1; i < array.length; i++) {
+        if (array[i] < minElement) {
+          minElement = array[i];
+          }
+      }
+      return minElement;
+  }
 }
